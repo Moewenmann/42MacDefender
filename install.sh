73802cf56f16a92ec39d59c3209d68e8bc894a06
Julian Möwenmann <moewenmann@protonmail.ch>
2024-02-11 19:18:31 +0100

    Upload first version of install.sh

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+curl https://github.com/Moewenmann/42MacDefender/blob/main/macdefender.sh > ~/MacDefender/macdefender.sh
+chmod +x ~/MacDefender/macdefender.sh
+cat <<EOT >> ~/.zshrc
+
+### ### ### ### MacDefender ### ### ### ###
+MACDEFENDER="active"
+
+TIMEKEEPER="inactive"
+
+LOCKTIME="600"
+
+~/MacDefender/macdefender.sh
+
+alias lock="~/MacDefender/macdefender.sh"
+alias macdefender="~/MacDefender/macdefender.sh"
+alias MacDefender="~/MacDefender/macdefender.sh"
+EOT
+
+echo ""
